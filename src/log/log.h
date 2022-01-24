cd71b748602190163aedffe1f65de41a006bd540
neov2 <neov2@illinois.edu>
2022-01-23 23:02:44 -0800

    [log] removed unnecessary comments

--- src/log/log.h
+++ src/log/log.h
@@ -11,11 +11,6 @@ typedef enum {
     NONE
 } log_level_t;
 
-//bool parse_p(bool perc);
-//bool parse_xud(bool perc, int64_t val, char op);
-//bool parse_c(bool perc, char c);
-//bool parse_s(bool perc, char* str);
-
 void print_level(log_level_t level);
 void log(log_level_t level, char* format, ...);
 
