b90a82d53c65d69c22e1507106e9f0b6b250decf
neov2 <neov2@illinois.edu>
2022-02-05 22:47:31 -0800

    [log] better logging, added support for both 32-bit and 64-bit values

--- src/log/log.c
+++ src/log/log.c
@@ -1,91 +1,144 @@
-#include <core/utils/utils.h>
-#include <string/string.h>
 #include <stdarg.h>
+#include <core/utils/utils.h>
 #include <core/macros.h>
+#include <dev/com.h>
+#include <string/string.h>
 
 #include "log.h"
-#include "com.h"
 
 /* 
  * the following are handlers 
  * each prints and returns the update to perc 
  */
 
-static inline bool parse_p(bool perc) {
-    if (perc) {
-        __com_putc('%');
+static inline void print_x(int32_t val) {
+    char buf[9];
+    buf[8] = '\0';
+
+    char* digits = "0123456789ABCDEF";
+    for (int i = 7; i >= 0; i--) {
+        buf[i] = digits[val & 0xF]; 
+        val >>= 4;
     }
-    return !perc;
+
+    __com_puts(buf);
 }
 
-static inline bool parse_xu(bool perc, int64_t val, char op) {
-    char temp[64];
-    memset(temp, 0, 64);
-    
-    if (perc) {
-        if (op == 'x') {
-            itoa(val, temp, 16, UNSIGNED);
-        } else if (op == 'u') {
-            itoa(val, temp, 10, UNSIGNED);
-        }
-        __com_write(temp);
-    } else {
-        __com_putc(op);
+static inline void print_lx(int64_t val) {
+    char buf[17];
+    buf[16] = '\0';
+
+    char* digits = "0123456789ABCDEF";
+    for (int i = 15; i >= 0; i--) {
+        buf[i] = digits[val & 0xF]; 
+        val >>= 4;
     }
 
-    return false;
+    __com_puts(buf);
 }
 
-static inline bool parse_d(bool perc, int64_t val) {
-    char temp[64];
-    memset(temp, 0, 64);
-    
-    if (perc) {
-        itoa(val, temp, 10, SIGNED);
-        __com_write(temp);
-    } else {
-        __com_putc('d');
+static inline void print_u(uint32_t val) {
+    char buf[11];
+    buf[10] = '\0';
+    int i = 10;
+    const int ascii_offset = '0';
+
+    if (!val) {
+        __com_puts(0);
+        return;
     }
 
-    return false;
+    while (val) {
+        buf[--i] = (val % 10) + ascii_offset;
+        val /= 10;
+    }
+
+    __com_puts((buf + i));
 }
 
-static inline bool parse_c(bool perc, char c) {
-    if (perc) {
-        __com_putc(c);
-    } else {
-        __com_putc('c');
+static inline void print_lu(uint64_t val) {
+    char buf[21];
+    buf[20] = '\0';
+    int i = 20;
+    const int ascii_offset = '0';
+
+    if (!val) {
+        __com_putc('0');
+        return;
     }
 
-    return false;
+    while (val) {
+        buf[--i] = (val % 10) + ascii_offset;
+        val /= 10;
+    }
+
+    __com_puts((buf + i));
 }
 
-static inline bool parse_s(bool perc, char* str) {
-    if (perc) {
-        __com_write(str);
-    } else {
-        __com_putc('s');
+static inline void print_d(int32_t val) {
+    char buf[12];
+    buf[11] = '\0';
+    int i = 11;
+    const int ascii_offset = '0';
+    bool neg = val < 0;
+
+    if (!val) {
+        __com_putc('0');
+        return;
     }
 
-    return false;
+    if (neg) val = -val;
+
+    while (val) {
+        buf[--i] = (val % 10) + ascii_offset;
+        val /= 10;
+    }
+
+    if (neg) buf[--i] = '-';
+
+    __com_puts((buf + i));
+}
+
+static inline void print_ld(int64_t val) {
+    char buf[21];
+    buf[20] = '\0';
+    int i = 20;
+    const int ascii_offset = '0';
+    bool neg = val < 0;
+
+    if (!val) {
+        __com_putc('0');
+        return;
+    }
+
+    if (neg) val = -val;
+
+    while (val) {
+        buf[--i] = (val % 10) + ascii_offset;
+        val /= 10;
+    }
+
+    if (neg) buf[--i] = '-';
+
+    __com_puts((buf + i));
 }
 
 /* print log level */
 void print_level(log_level_t level) {
     switch (level) {
-        case LOG_INFO:
-            __com_write("[INFO]: ");
+        case INFO:
+            __com_puts("[INFO]: ");
             break;
-        case LOG_TODO: 
-            __com_write("[TODO]: ");
+        case TODO: 
+            __com_puts("[TODO]: ");
             break;
-        case LOG_ERROR:
-            __com_write("[ERROR]: ");
+        case ERROR:
+            __com_puts("[ERROR]: ");
             break;
         case NONE:
             break;
         default:
-            __com_write("[... no log level ...] ");
+            __com_puts("[... no log level ...] ");
     }
 }
 
@@ -96,53 +149,87 @@ void print_level(log_level_t level) {
  * unsigned long suffixes (ex. 69l) or cast to int64_t or 
  * uint64_t respectively
  * 
- * TODO: remove warnings 
- * 
- * All integer values are assumed to be int64_t
  * %% = print '%'
- * %x = print value as hexadecimal
- * %u = print value as uint64_t
- * %d = print value as int64_t
- * %c = print value as uint8_t
+ * %c = print value as char
  * %s = print value as string
+ * %x = print value as 32-bit hexadecimal
+ * %lx = print value as 64-bit hexadecimal
+ * %u = print value as 32-bit unsigned
+ * %lu = print value as 64-bit unsigned hexadecimal
+ * %d = print value as 32-bit decimal
+ * %ld = print value as 64-bit decimal
+ * 
+ * returns 0 if parsing was successful and -1 if failed
  */
-void log(log_level_t level, char* format, ...) {
+int log_impl(log_level_t level, char* format, ...) {
     print_level(level);
 
     va_list args;
     va_start(args, format);
 
-    /* '%' detected while parsing */
-    bool perc = false;
     char* buf = format;
-    void* curr_arg;
 
     while (*buf != '\0') {
         switch (*buf) {
             case '%':
-                perc = parse_p(perc);
+                buf++;
+
+                if (*buf == '\0')
+                    return -1;
+
+                switch (*buf) {
+                    case '%':
+                        __com_putc('%');
+                        break;
+                    case 'c':
+                        __com_putc((char) va_arg(args, int));
+                        break;
+                    case 's':
+                        __com_puts((char*) va_arg(args, char*));
+                        break;
+                    case 'x':
+                        print_x((int32_t) va_arg(args, int32_t));
+                        break;
+                    case 'u':
+                        print_u((uint32_t) va_arg(args, uint32_t));
+                        break;
+                    case 'd':
+                        print_d((int32_t) va_arg(args, int32_t));
+                        break;
+                    case 'l':
+                        buf++;
+
+                        if (*buf == '\0')
+                            return -1;
+
+                        switch (*buf) {
+                            case 'x':
+                                print_lx((int64_t) va_arg(args, int64_t));
+                                break;
+                            case 'u':
+                                print_lu((uint64_t) va_arg(args, uint64_t));
+                                break;
+                            case 'd':
+                                print_ld((int64_t) va_arg(args, int64_t));
+                                break;
+                            default: 
+                                return -1;
+                        }
+                        
+                        break;
+
+                    default:
+                        return -1;
+                }
+                
                 break;
-            case 'x':
-            case 'u':
-                if (perc) curr_arg = (void*) va_arg(args, uint64_t);
-                perc = parse_xu(perc, (uint64_t) curr_arg, *buf);
-                break;
-            case 'd':
-                if (perc) curr_arg = (void*) va_arg(args, int64_t);
-                perc = parse_d(perc, (int64_t) curr_arg);
-                break;
-            case 'c':
-                if (perc) curr_arg = (void*) va_arg(args, int);
-                perc = parse_c(perc, (char) curr_arg);
-                break;
-            case 's':
-                if (perc) curr_arg = va_arg(args, void*);
-                perc = parse_s(perc, (char*) curr_arg);
-                break;
+
             default:
                 __com_putc(*buf);
         }
 
         buf++;
     } 
+
+    return 0;
 }
--- src/log/log.h
+++ src/log/log.h
@@ -3,14 +3,19 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-
 typedef enum {
-    LOG_INFO,
-    LOG_TODO,
-    LOG_ERROR,
+    INFO,
+    TODO,
+    ERROR,
     NONE
 } log_level_t;
 
 void print_level(log_level_t level);
-void log(log_level_t level, char* format, ...);
+int log_impl(log_level_t level, char* format, ...);
+
+#define info(format, ...)   log_impl(INFO, format, ## __VA_ARGS__) 
+#define todo(format, ...)   log_impl(TODO, format, ## __VA_ARGS__)
+#define error(format, ...)  log_impl(ERROR, format, ## __VA_ARGS__)
+#define log(format, ...)    log_impl(NONE, format, ## __VA_ARGS__)
+
 
